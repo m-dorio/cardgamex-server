67bd9b85af135c62d948d7286d43cd9db5eb4a1c
Michael Angelo Dorio <michaelangelo.dorio@gmail.com>
2025-03-11 00:17:32 +0800

    configure to add lobby and change game state

--- server.js
+++ server.js
@@ -19,20 +19,46 @@ io.on("connection", (socket) => {
   console.log("A user connected:", socket.id);
 
   socket.on("join-game", (roomId, playerData) => {
-    if (!rooms[roomId]) {
-      rooms[roomId] = { players: {} };
-    }
-    rooms[roomId].players[socket.id] = playerData;
+    if (!rooms[roomId]) rooms[roomId] = { players: {}, playAgainVotes: 0 };
 
+    if (Object.keys(rooms[roomId].players).length >= 2) {
+      socket.emit("error-message", "Room is full.");
+      return;
+    }
+
+    rooms[roomId].players[socket.id] = playerData;
     socket.join(roomId);
     io.to(roomId).emit("update-players", rooms[roomId].players);
   });
 
   socket.on("attack", ({ roomId, attackerId, damage }) => {
-    const opponentId = Object.keys(rooms[roomId].players).find(id => id !== attackerId);
+    const opponentId = Object.keys(rooms[roomId].players).find(
+      (id) => id !== attackerId
+    );
     if (opponentId) {
       io.to(opponentId).emit("receive-attack", damage);
     }
+  });
+
+  socket.on("vote-play-again", (roomId) => {
+    if (rooms[roomId]) {
+      rooms[roomId].playAgainVotes += 1;
+      io.to(roomId).emit("play-again-vote", rooms[roomId].playAgainVotes);
+
+      if (rooms[roomId].playAgainVotes >= 2) {
+        rooms[roomId].playAgainVotes = 0;
+        io.to(roomId).emit("reset-game");
+      }
+    }
+  });
+
+  socket.on("leave-room", (roomId) => {
+    if (rooms[roomId] && rooms[roomId].players[socket.id]) {
+      delete rooms[roomId].players[socket.id];
+      io.to(roomId).emit("update-players", rooms[roomId].players);
+      io.to(roomId).emit("player-left");
+    }
+    socket.leave(roomId);
   });
 
   socket.on("disconnect", () => {
